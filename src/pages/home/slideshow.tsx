53559966e1428c5f5b28d80e66c65390ef9146a0
Yrla Hackzell <yrla.hackzell@hotmail.com>
2024-01-17 10:29:03 +0100

    add slideshow text

--- src/pages/home/slideshow.tsx
+++ src/pages/home/slideshow.tsx
@@ -1,62 +1,72 @@
-import React, { useState } from 'react'
-import './home.css';
+import React, { useState } from 'react';
+import styles from './slideshow.module.css';
 import Btn from '../../components/button/btn';
-import image from '../../images/slide1.jpg';
-
 
 export default function Slideshow() {
-    const textItems = [
-        {
-          title: 'Dinner',
-          description: 'Lorem ipsum dolor sit amet consectetur. Libero ullamcorper netus fringilla ipsum ante dolor.',
-        },
-        {
-          title: 'Lunch',
-          description: 'Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.',
-        },
-        // test push
-      ];
-    
-      const [currentTextIndex, setCurrentTextIndex] = useState(0);
-    
-      const nextSlide = () => {
-        const newIndex = (currentTextIndex + 1) % textItems.length;
-        setCurrentTextIndex(newIndex);
-      };
-    
-      const prevSlide = () => {
-        const newIndex = (currentTextIndex - 1 + textItems.length) % textItems.length;
-        setCurrentTextIndex(newIndex);
-      };
-    
-    return (
-        <div className='slideshow'>
-                <div className='left-side'>
-            
-            <div className='image-container'>
-                 <img src={image} alt='hi'></img>
-                 </div>
-     
-                </div>
-                <div className="slider">
-      <button onClick={prevSlide}>Previous</button>
-      <div className="slide-wrapper">
-        {textItems.map((item, index) => (
-          <div
-            key={index}
-            className={`slide ${index === currentTextIndex ? 'active' : ''}`}
-          >
-            <div className='text-item'>
-              <h2>{item.title}</h2>
-              <p>{item.description}</p>
-              {/* You can include Btn component here */}
-            </div>
-          </div>
-        ))}
-      </div>
-      <button onClick={nextSlide}>Next</button>
+  const textItems = [
+    {
+      id: 0,
+      title: 'Dinner',
+      description: 'Lorem ipsum dolor sit amet consectetur. Libero ullamcorper netus fringilla ipsum ante dolor.',
+    },
+    {
+      id: 1,
+      title: 'Lunch',
+      description: 'Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.',
+    },
+    {
+      id: 2,
+      title: 'Lunch',
+      description: 'Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.',
+    },
+  ];
+
+  const [currentTextIndex, setCurrentTextIndex] = useState(1);
+
+  const handleButtonClick = (direction:string) => {
+    // if cuurrent 
+    if (direction === 'up' ) {
+      if(currentTextIndex > 1)
+      setCurrentTextIndex(currentTextIndex - 1);
+    else  {
+      setCurrentTextIndex(3)
+    }
+
+    } 
+    else if (direction === 'down' ) {
+      if(currentTextIndex < textItems.length ){
+      setCurrentTextIndex(currentTextIndex + 1);}
+      else{
+        setCurrentTextIndex(1)
+      }
+    }
+
+  };
+
+  return (
+    <div className={styles.slider}>
+<a href={`#slide-${currentTextIndex}`} onClick={() => handleButtonClick('up')}>
+  Previous
+</a>
+
+<a href={`#slide-${currentTextIndex}`} onClick={() => handleButtonClick('down')}>
+  Next
+</a>
+
+
+      <div className={styles.slides}>
+<div id="slide-1">
+      1
     </div>
-     
-        </div>
-    )
+    <div id="slide-2">
+      2
+    </div>
+    <div id="slide-3">
+      3
+    </div>
+
+
+      </div>
+    </div>
+  );
 }
